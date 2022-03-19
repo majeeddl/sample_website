905115e127faf6a0a288cb577665db22e715a0a9
majeed <majeed.dl@gmail.com>
2022-03-19 18:25:14 +0300

    fix error

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import "antd/dist/antd.css";
+import "antd/dist/antd.min.css";
 import "./index.css";
 
 
